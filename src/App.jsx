cbd3f07d5085eb1674c6014f782635cced58da13
Rubika-k <krubika2004@gmail.com>
2025-04-23 22:00:28 +0530

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Navbar from "./Components/Navbar";
+import Home from "./Pages/Home";
+import AboutUs from "./Pages/AboutUs";
+import Faq from "./Pages/Faq";
+import Contact from "./Pages/Contact";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<AboutUs />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Faq" element={<Faq />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
